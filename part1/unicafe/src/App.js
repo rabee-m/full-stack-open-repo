f1547fc357c11eb1c30dd0367e9d7227e550b585
Rabee <rabee@Talals-MacBook-Air.local>
2022-07-12 15:00:50 -0400

    add boilerplate for unicafe

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, text}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <h1>statistics</h1>
+    </div>
+  )
+}
+
+export default App
